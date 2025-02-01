330474549da34aeadce0c323e47e49499210bc81
Denismaka <makadenis370@gmail.com>
2025-02-01 19:18:52 +0100

    configuration de phpmailer pour la gestion de la newsletter

---
+++ assets/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'denis-maka/assets',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2bc95567e9c4f60b2ebcd4380129e6addde892e5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'denis-maka/assets' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2bc95567e9c4f60b2ebcd4380129e6addde892e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.3',
+            'version' => '6.9.3.0',
+            'reference' => '2f5c94fe7493efc213f643c23b1b1c249d40f47e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
