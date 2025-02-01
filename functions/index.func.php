bbe2f6b3ffc9b31af6346b70b503758c69db7942
Denismaka <makadenis370@gmail.com>
2025-02-01 17:36:03 +0100

    debogage d'un ligne de code

--- functions/index.func.php
+++ functions/index.func.php
@@ -1,85 +1,85 @@
 
 <?php
 
-session_start();
+// session_start();
 
 if (isset($_POST['login'])) {
 
-$email_user = htmlspecialchars(trim($_POST['email_user']));
-$password = htmlspecialchars(trim($_POST['password_user']));
+    $email_user = htmlspecialchars(trim($_POST['email_user']));
+    $password = htmlspecialchars(trim($_POST['password_user']));
 
 
-if (!empty($email_user) && !empty($password)) {
+    if (!empty($email_user) && !empty($password)) {
 
 
 
 
-    if (adminConnect($email_user) == 1) {
+        if (adminConnect($email_user) == 1) {
 
-        $pas = adminInfo($email_user, $password);
+            $pas = adminInfo($email_user, $password);
 
-        $password_hasher = sha1($password);
+            $password_hasher = sha1($password);
 
-        if ($password_hasher == $pas['password_user']) {
+            if ($password_hasher == $pas['password_user']) {
 
-            $adminInfo = adminInfo($email_user, $password);
-            $_SESSION['id_user'] = $adminInfo['id_user'];
-            $_SESSION['email_user'] = $adminInfo['email_user'];
-            $_SESSION['email_user'] = $adminInfo['email_user'];
-            $_SESSION['id_grade'] = $adminInfo['id_grade'];
-            $_SESSION['confirmation_user'] = $adminInfo['confirmation_user'];
+                $adminInfo = adminInfo($email_user, $password);
+                $_SESSION['id_user'] = $adminInfo['id_user'];
+                $_SESSION['email_user'] = $adminInfo['email_user'];
+                $_SESSION['email_user'] = $adminInfo['email_user'];
+                $_SESSION['id_grade'] = $adminInfo['id_grade'];
+                $_SESSION['confirmation_user'] = $adminInfo['confirmation_user'];
 
-            if ($_SESSION['id_grade'] == 3) {
-                header('Location: card.php?id_user=' . $_SESSION['id_user']);
+                if ($_SESSION['id_grade'] == 3) {
+                    header('Location: card.php?id_user=' . $_SESSION['id_user']);
+                } else {
+                    header('Location: dashboard.php?page=home&id_user=' . $_SESSION['id_user']);
+                }
             } else {
-                header('Location: dashboard.php?page=home&id_user=' . $_SESSION['id_user']);
+                $error = "<font color='red'>Mot de passe incorrect !</font>";
             }
         } else {
-            $error = "<font color='red'>Mot de passe incorrect !</font>";
+
+            $error = "<font color='red'>Cet utilisateur n'existe pas ! ";
         }
     } else {
-
-        $error = "<font color='red'>Cet utilisateur n'existe pas ! ";
+        $error = "<font color='red'>Tous les champs ne sont pas remplis !</font>";
     }
-} else {
-    $error = "<font color='red'>Tous les champs ne sont pas remplis !</font>";
-}
 }
 
 function adminConnect($email_user)
 {
-$password = htmlspecialchars(trim($_POST['password_user']));
-global $db;
+    $password = htmlspecialchars(trim($_POST['password_user']));
+    global $db;
 
-$a = ['email_user' => $email_user];
+    $a = ['email_user' => $email_user];
 
 
-$sql = "SELECT * FROM users WHERE email_user = :email_user ";
-$req = $db->prepare($sql);
-$req->execute($a);
+    $sql = "SELECT * FROM users WHERE email_user = :email_user ";
+    $req = $db->prepare($sql);
+    $req->execute($a);
 
-$exist = $req->rowCount();
+    $exist = $req->rowCount();
 
-return $exist;
+    return $exist;
 }
 
 function adminInfo($email_user, $password)
 {
 
-global $db;
+    global $db;
 
-$a = [
+    $a = [
 
-    'email_user'     => $email_user,
-    'password_user'  => sha1($password)
-];
+        'email_user'     => $email_user,
+        'password_user'  => sha1($password)
+    ];
 
 
-$sql = "SELECT * FROM users WHERE email_user = :email_user AND password_user = :password_user";
-$req = $db->prepare($sql);
-$req->execute($a);
+    $sql = "SELECT * FROM users WHERE email_user = :email_user AND password_user = :password_user";
+    $req = $db->prepare($sql);
+    $req->execute($a);
 
-$exist = $req->fetch();
+    $exist = $req->fetch();
 
-return $exist;
+    return $exist;
 }
