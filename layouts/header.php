b95a1a6822aa1430f3ccaa146f56206f27854d23
Denismaka <makadenis370@gmail.com>
2025-02-01 17:37:24 +0100

    correction de la navbar

--- layouts/header.php
+++ layouts/header.php
@@ -1,6 +1,6 @@
-<?php 
-session_start(); // Démarre la session
-include 'functions/db.php'; 
+<?php
+// session_start(); // Démarre la session
+include 'functions/db.php';
 
 // Vérifie si l'utilisateur est connecté et a le grade 1
 if (isset($_SESSION['user_id']) && $_SESSION['grade'] == 1) {
@@ -11,6 +11,7 @@ if (isset($_SESSION['user_id']) && $_SESSION['grade'] == 1) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -19,6 +20,7 @@ if (isset($_SESSION['user_id']) && $_SESSION['grade'] == 1) {
     <link rel="stylesheet" href="../assets/fonts/css/all.min.css" />
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css" />
 </head>
+
 <body>
     <!-- ############## -->
     <!-- Header -->
@@ -31,7 +33,7 @@ if (isset($_SESSION['user_id']) && $_SESSION['grade'] == 1) {
 
                 if ($_SESSION['id_grade'] == 3) {
             ?>
-                    
+
                 <?php
                 } else {
                 ?>
@@ -80,28 +82,34 @@ if (isset($_SESSION['user_id']) && $_SESSION['grade'] == 1) {
                 text-decoration: none;
                 text-transform: capitalize;
             }
+
             #nom_user:hover {
                 color: var(--orange);
                 transition: 0.2s ease-in-out;
             }
+
             #deconnexion {
-              margin-top: 15px;
+                margin-top: 15px;
             }
+
             #dashboard {
-              margin-left: 300px;
-              margin-bottom: 15px;
-              list-style: none;
-              text-decoration: none;
-              color: var(--white);
-              font-weight: 600;
+                margin-left: 300px;
+                margin-bottom: 15px;
+                list-style: none;
+                text-decoration: none;
+                color: var(--white);
+                font-weight: 600;
             }
+
             #dashboard:hover {
-              color: var(--orange);
+                color: var(--orange);
             }
+
             #dns {
-              margin-top: 15px;
+                margin-top: 15px;
             }
         </style>
     </header>
 </body>
+
 </html>
